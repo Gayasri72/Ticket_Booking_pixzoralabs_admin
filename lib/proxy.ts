1e4751438880db98259e2b27f9544af1a808087c
Gayasri Pethum Kumarana <119282699+Gayasri72@users.noreply.github.com>
2025-12-16 07:17:19 +0530

    Delete lib directory

--- lib/proxy.ts
+++
@@ -1,9 +0,0 @@
-import { auth as middlewareAuth } from "@/auth";
-
-/**
- * Re-export auth for use in API routes
- * In Next.js 16 with Auth.js v5, this should automatically have access to session context
- */
-export const auth = middlewareAuth;
-
-export default middlewareAuth;
